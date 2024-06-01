b47adb7a0627d97b6126459380e765e8e36fa650
HyunJaeyeon <bomul0109@gmail.com>
2024-06-01 22:44:33 +0900

    Initial Commit

---
+++ MungContest/MungContestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MungContestApp.swift
+//  MungContest
+//
+//  Created by Hyun Jaeyeon on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MungContestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
