f3c4d33fe98cee10fb5594a116988382d1eb9e2c
HyunJaeyeon <bomul0109@gmail.com>
2024-06-02 05:01:08 +0900

    feat: image auto slide 구현
    - Timer 설정해서 자동으로 넘어갈 수 있게
    - TabView로 이미지 슬라이드 구현

--- MungContest/View/StandbyView.swift
+++ MungContest/View/StandbyView.swift
@@ -1,25 +1,40 @@
-//
-//  StandbyView.swift
-//  MungContest
-//
-//  Created by Woowon Kang on 6/1/24.
-//
-
 import SwiftUI
 
 struct StandbyView: View {
     @Environment(NavigationManager.self) var navigationManager
+    private let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    @State private var index = 0
+    @State private var selectedNum: String = ""
+    
+    private let images: [String] = ["jen", "junyo", "mars", "won"]
     
     var body: some View {
-        VStack {
-            Button("대회 메인 화면으로") {
-                navigationManager.push(to: .main)
+        //                VStack {
+        //                    Button("대회 메인 화면으로") {
+        //                        navigationManager.push(to: .main)
+        //                    }
+        //                }
+        TabView(selection: $selectedNum) {
+            ForEach(images, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFit()
             }
         }
+        .tabViewStyle(.page)
+        .onReceive(timer, perform: { _ in
+            withAnimation {
+                index = (index + 1) % images.count
+                selectedNum = images[index]
+            }
+        })
     }
 }
 
-#Preview {
-    StandbyView()
-        .environment(NavigationManager())
+struct StandbyView_Previews: PreviewProvider {
+    static var previews: some View {
+        StandbyView()
+            .environment(NavigationManager())
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
 }
