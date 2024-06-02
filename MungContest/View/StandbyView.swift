6f942dc5314e7984bfba2ed1d6f9c9e30e886c13
HyunJaeyeon <bomul0109@gmail.com>
2024-06-02 20:03:58 +0900

    feat: 각 이미지의 위치별로 크기를 다르게 설정
    - getScale함수 추가
    - GeometryReader 사용

--- MungContest/View/StandbyView.swift
+++ MungContest/View/StandbyView.swift
@@ -6,7 +6,7 @@ struct StandbyView: View {
     private let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
     @State private var currentIndex = 0
     
-    private let images: [String] = ["jen", "junyo", "mars", "won"]
+    private let images: [String] = ["jen", "junyo", "mars", "won", "hash"]
     @State var dummieData : [Player] = [
         Player(name: "준요", profileImage: Data(), comment: "내가 젠은 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0),
         Player(name: "원", profileImage: Data(), comment: "내가 준요는 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0),
@@ -21,22 +21,29 @@ struct StandbyView: View {
         //                        navigationManager.push(to: .main)
         //                    }
         //                }
-        ScrollViewReader { scrollProxy in // needed to scroll programmatically
+        ScrollViewReader { scrollProxy in
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .center, spacing: 0) {
                     ForEach(0..<dummieData.count, id: \.self) { i in
-                        VStack{
-                            Image(images[i % images.count])
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 400, height: 400)
-                                .clipShape(RoundedRectangle(cornerRadius: 260))
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 260)
-                                        .stroke(.yellow, lineWidth: 6)
-                                }
+                        GeometryReader { geometry in
+                            let scale = getScale(proxy: geometry)
+                            VStack{
+                                Image(images[i % images.count])
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 400 * scale, height: 400 * scale)
+                                    .clipShape(RoundedRectangle(cornerRadius: 260 * scale))
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 260 * scale)
+                                            .stroke(i == currentIndex ? Color.yellow : Color.gray, lineWidth: 10 * scale)
+                                    }
+                                    .border(.white)
+                            }
+                            .frame(width: 400, height: 400)
+                            .border(.white)
                         }
                     }
+                    .frame(width: 400, height: 400)
                 }
                 .onReceive(timer) { _ in
                     currentIndex = currentIndex < dummieData.count - 1 ? currentIndex + 1 : 0
@@ -45,7 +52,19 @@ struct StandbyView: View {
                     }
                 }
             }
-        }}
+        }
+    }
+    //MARK: 스케일 설정 함수
+    private func getScale(proxy: GeometryProxy) -> CGFloat {
+        let midPoint: CGFloat = 200
+        let viewFrame = proxy.frame(in: .global)
+        let distanceFromCenter = abs(viewFrame.midX - UIScreen.main.bounds.width / 2)
+        let minScale: CGFloat = 0.76
+        let maxScale: CGFloat = 1.0
+        
+        return max(maxScale - (distanceFromCenter / midPoint) * (maxScale - minScale), minScale)
+//        return max(maxScale, minScale)
+    }
 }
 
 struct StandbyView_Previews: PreviewProvider {
