b78203cb970d65c1873a682dbac4fef3e135f86e
HyunJaeyeon <bomul0109@gmail.com>
2024-06-02 15:37:25 +0900

    feat: 이미지 전환뷰를 ScrollView로 수정
    - 원래는 TabView-> currentIndex들이 안보이는 이슈가 있었음
    - scrollView를 이용해 current 외의 이미지들도 보이게 함

--- MungContest/View/StandbyView.swift
+++ MungContest/View/StandbyView.swift
@@ -2,11 +2,18 @@ import SwiftUI
 
 struct StandbyView: View {
     @Environment(NavigationManager.self) var navigationManager
+    
     private let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
-    @State private var index = 0
-    @State private var selectedNum: String = ""
+    @State private var currentIndex = 0
     
     private let images: [String] = ["jen", "junyo", "mars", "won"]
+    @State var dummieData : [Player] = [
+        Player(name: "준요", profileImage: Data(), comment: "내가 젠은 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0),
+        Player(name: "원", profileImage: Data(), comment: "내가 준요는 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0),
+        Player(name: "젠", profileImage: Data(), comment: "내가 원은 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0),
+        Player(name: "해시", profileImage: Data(), comment: "내가 젠은 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0),
+        Player(name: "마스", profileImage: Data(), comment: "내가 해시는 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0)
+    ]
     
     var body: some View {
         //                VStack {
@@ -14,21 +21,31 @@ struct StandbyView: View {
         //                        navigationManager.push(to: .main)
         //                    }
         //                }
-        TabView(selection: $selectedNum) {
-            ForEach(images, id: \.self) {
-                Image($0)
-                    .resizable()
-                    .scaledToFit()
+        ScrollViewReader { scrollProxy in // needed to scroll programmatically
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .center, spacing: 0) {
+                    ForEach(0..<dummieData.count, id: \.self) { i in
+                        VStack{
+                            Image(images[i % images.count])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 400, height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 260))
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 260)
+                                        .stroke(.yellow, lineWidth: 6)
+                                }
+                        }
+                    }
+                }
+                .onReceive(timer) { _ in
+                    currentIndex = currentIndex < dummieData.count - 1 ? currentIndex + 1 : 0
+                    withAnimation {
+                        scrollProxy.scrollTo(currentIndex, anchor: .center) // scroll to next .id
+                    }
+                }
             }
-        }
-        .tabViewStyle(.page)
-        .onReceive(timer, perform: { _ in
-            withAnimation {
-                index = (index + 1) % images.count
-                selectedNum = images[index]
-            }
-        })
-    }
+        }}
 }
 
 struct StandbyView_Previews: PreviewProvider {
@@ -36,5 +53,6 @@ struct StandbyView_Previews: PreviewProvider {
         StandbyView()
             .environment(NavigationManager())
             .previewInterfaceOrientation(.landscapeLeft)
+            .preferredColorScheme(.dark)
     }
 }
