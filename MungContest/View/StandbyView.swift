ffbb95ccfbb95d9703d76639633f7ac6cc92e3ae
HyunJaeyeon <bomul0109@gmail.com>
2024-06-03 00:10:21 +0900

    Chore: 사진 그리드 수정 및 버튼, 상단바 추가

--- MungContest/View/StandbyView.swift
+++ MungContest/View/StandbyView.swift
@@ -15,43 +15,79 @@ struct StandbyView: View {
         Player(name: "마스", profileImage: Data(), comment: "내가 해시는 이긴다 ㅋㅋ", defaultHeartrate: 0, heartrates: [0, 0, 0, 0], differenceHeartrates: [0, 0, 0, 0], resultHeartrate: 0)
     ]
     
+    static let dateformat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY년 M월 d일"
+        return formatter
+    }()
+    
+    var today = Date()
+    
     var body: some View {
+        
         //                VStack {
         //                    Button("대회 메인 화면으로") {
         //                        navigationManager.push(to: .main)
         //                    }
         //                }
-        ScrollViewReader { scrollProxy in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: .center, spacing: 0) {
-                    ForEach(0..<dummieData.count, id: \.self) { i in
-                        GeometryReader { geometry in
-                            let scale = getScale(proxy: geometry)
-                            VStack{
-                                Image(images[i % images.count])
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 400 * scale, height: 400 * scale)
-                                    .clipShape(RoundedRectangle(cornerRadius: 260 * scale))
-                                    .overlay {
-                                        RoundedRectangle(cornerRadius: 260 * scale)
-                                            .stroke(i == currentIndex ? Color.yellow : Color.gray, lineWidth: 10 * scale)
-                                    }
-                                    .border(.white)
+        VStack{
+            Text("\(today)")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .frame(width: 978, height: 76)
+                .background(.gray)
+                .clipShape(Capsule())
+                .padding(.bottom, 64)
+            
+            ScrollViewReader { scrollProxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .center, spacing: 0) {
+                        ForEach(0..<dummieData.count, id: \.self) { i in
+                            GeometryReader { geometry in
+                                let scale = getScale(proxy: geometry)
+                                VStack{
+                                    Image(images[i % images.count])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 260 * scale, height: 260 * scale)
+                                        .clipShape(Circle())
+                                        .overlay {
+                                            Circle()
+                                                .stroke(i == currentIndex ? Color.yellow : Color.gray, lineWidth: 10 * scale)
+                                        }
+                                        .border(.white)
+                                }
+                                .frame(width: 280, height: 280)
+                                .border(.white)
                             }
-                            .frame(width: 400, height: 400)
-                            .border(.white)
                         }
+                        .frame(width: 280, height: 280)
                     }
-                    .frame(width: 400, height: 400)
-                }
-                .onReceive(timer) { _ in
-                    currentIndex = currentIndex < dummieData.count - 1 ? currentIndex + 1 : 0
-                    withAnimation {
-                        scrollProxy.scrollTo(currentIndex, anchor: .center) // scroll to next .id
+                    .onReceive(timer) { _ in
+                        currentIndex = currentIndex < dummieData.count - 1 ? currentIndex + 1 : 0
+                        withAnimation {
+                            scrollProxy.scrollTo(currentIndex, anchor: .center) // scroll to next .id
+                        }
                     }
                 }
             }
+            
+            Button(action: {
+            }, label: {
+                Text("심박수 입력하기")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 14)
+                    .background(.yellow)
+                    .clipShape(Capsule())
+                    .overlay(
+                        Capsule()
+                            .stroke(.pink, lineWidth: 1)
+                    )
+            })
+            .padding(.top, 50)
         }
     }
     //MARK: 스케일 설정 함수
@@ -63,7 +99,6 @@ struct StandbyView: View {
         let maxScale: CGFloat = 1.0
         
         return max(maxScale - (distanceFromCenter / midPoint) * (maxScale - minScale), minScale)
-//        return max(maxScale, minScale)
     }
 }
 
