b32759c7eba63dd4626641d134a734d1714fc457
won <mosiccan@gmail.com>
2024-08-16 16:20:53 +0900

    Feat: #35 contentView에서 swiftdata 초기화

--- MungContest/View/ContentView.swift
+++ MungContest/View/ContentView.swift
@@ -6,15 +6,19 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @State private var navigationManager = NavigationManager()
+    @Query private var defaultPlayers: [Player]
+    @Environment(\.modelContext) var modelContext
     
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
             VStack {
                 Text("여기는 실제로 보이지 않는 화면입니다.")
                 Button("대회 설정 화면으로") {
+                    resetPlayers()
                     navigationManager.push(to: .plan)
                 }
             }
@@ -24,6 +28,21 @@ struct ContentView: View {
         }
         .environment(navigationManager)
         
+    }
+    
+    private func resetPlayers() {
+        for player in defaultPlayers {
+            modelContext.delete(player)
+        }
+        saveContext()
+    }
+    
+    private func saveContext() {
+        do {
+            try modelContext.save()
+        } catch {
+            print("Failed to save context: \(error.localizedDescription)")
+        }
     }
 }
 
