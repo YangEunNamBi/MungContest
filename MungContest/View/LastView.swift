f5451b26c34572e34652a6083e355559e1b15829
SANGDOLEE <xkdxh625@naver.com>
2024-08-17 20:22:14 +0900

    Feat: LastView View 임시 commit

---
+++ MungContest/View/LastView.swift
@@ -0,0 +1,18 @@
+//
+//  LastView.swift
+//  MungContest
+//
+//  Created by 이상도 on 8/17/24.
+//
+
+import SwiftUI
+
+struct LastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LastView()
+}
