90374b5da3cc81aa8e401cb14b6d264c1a9b251a
won <mosiccan@gmail.com>
2024-08-19 00:13:27 +0900

    Feat: #47 초기화 버튼 구현

--- MungContest/View/ContentView.swift
+++ MungContest/View/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftData
 
 struct ContentView: View {
     @State private var navigationManager = NavigationManager()
-    @Query private var defaultPlayers: [Player]
     @Environment(\.modelContext) var modelContext
     
     var body: some View {
@@ -18,7 +17,6 @@ struct ContentView: View {
             VStack {
                 Text("여기는 실제로 보이지 않는 화면입니다.")
                 Button("대회 설정 화면으로") {
-                    resetPlayers()
                     navigationManager.push(to: .plan)
                 }
             }
@@ -28,21 +26,6 @@ struct ContentView: View {
         }
         .environment(navigationManager)
         
-    }
-    
-    private func resetPlayers() {
-        for player in defaultPlayers {
-            modelContext.delete(player)
-        }
-        saveContext()
-    }
-    
-    private func saveContext() {
-        do {
-            try modelContext.save()
-        } catch {
-            print("Failed to save context: \(error.localizedDescription)")
-        }
     }
 }
 
